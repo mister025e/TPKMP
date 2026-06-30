f3719766c44afca59efcdef06bc8b5d899a25f57
mister025e <mister025.2.0@gmail.com>
2026-06-30 15:06:03 +0200

    Test 2

--- shared/src/commonTest/kotlin/com/example/tpkmp/MainViewModelTest.kt
+++ shared/src/commonTest/kotlin/com/example/tpkmp/MainViewModelTest.kt
@@ -1,10 +1,17 @@
 package com.example.tpkmp
 
+import com.example.tpkmp.data.remote.WeatherApiDataSource
+import com.example.tpkmp.presentation.viewmodel.MainViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import kotlin.test.Test
+
 class MainViewModelTest {
 
     // Test qui réalise une vraie requête réseau
-    // Mis en commentaire pour que la CI passe sans dépendre d'une API externe
-    /*
     @Test
     fun testLoadWeathersRealRequest() {
         val client = HttpClient {
@@ -20,5 +27,4 @@ class MainViewModelTest {
         // Dans un test réel, on utiliserait runTest pour attendre la fin de la coroutine
         // et vérifier que viewModel.dataList n'est plus vide.
     }
-    */
 }
